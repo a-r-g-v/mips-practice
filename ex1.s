b6cb3f065c48084db050c337e0b99d897b4b975f
greyia <info@thun.ws>
2014-10-08 17:08:42 +0900

    Ex1 and Ex2

---
+++ ex1.s
@@ -0,0 +1,7 @@
+	.text
+	.globl main
+main:	li	$t0,	0x000f
+	li	$t1,	0x0001
+	add	$t2,	$t0,	$t1
+	jr	$ra
+
---
+++ ex2.s
@@ -0,0 +1,8 @@
+	.data
+str:	.asciiz	"Hello World\n"
+	.text
+	.globl main
+main:	li	$v0,	4
+	la	$a0,	str
+	syscall
+	jr	$ra
